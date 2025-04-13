0c956d8f2daee4bc38a680dcc7e234189fe4a63d
Noelle Mburu <148197183+noelle-mburu@users.noreply.github.com>
2025-04-13 21:07:54 +0300

    Update SQL Assignment 1.sql

--- SQL Assignment 1.sql
+++ SQL Assignment 1.sql
@@ -1,55 +1,55 @@
+select *
+from assignment.international_debt_with_missing_values idwmv ;
+
 --What is the total amount of debt owed by all countries in the dataset?
 select
- CAST(SUM(debt) / 1000000 AS numeric(12, 2)) AS total_debt_in_millions
-from "assignment".international_debt_with_missing_values idwmv;
+ CAST(sum(debt)/1000000 as numeric (12,2)) as Total_debt_in_millions
+from assignment.international_debt_with_missing_values;
+ 
 --How many distinct countries are recorded in the dataset?
 select 
- count(distinct country_name) as unique_countries
-from international_debt_with_missing_values idwmv ;
-
+ Count (distinct country_name) as Total_distinct_countries
+from assignment.international_debt_with_missing_values;
+ 
 --What are the distinct types of debt indicators, and what do they represent?
-select 
-distinct indicator_name
-from international_debt_with_missing_values idwmv;
+
+select distinct indicator_name
+from assignment.international_debt_with_missing_values idwmv;
 
 --Which country has the highest total debt, and how much does it owe?
-select distinct country_name,
-sum(debt) as total_debt
-from international_debt_with_missing_values idwmv
-group by country_name order by sum(debt) desc
-limit 2;
-
+select country_name,
+ sum(debt) as Total_debt_per_country
+from assignment.international_debt_with_missing_values 
+ group by country_name
+ order by sum(debt) desc
+ limit 2;
+ 
 --What is the average debt across different debt indicators?
-select distinct(indicator_name),
-avg (debt)
-from international_debt_with_missing_values idwmv 
-group by indicator_name;
-
+select indicator_name,
+ cast(avg(debt)/1000000 as numeric(10, 2)) as average_debt_in_millions
+from assignment.international_debt_with_missing_values
+ group by indicator_name
+ order by avg(debt) asc;
+ 
 --Which country has made the highest amount of principal repayments?
-select distinct country_name,
-sum(debt)
-from international_debt_with_missing_values idwmv 
-where indicator_name like '%Principal repayments%' 
-group by country_name
-having sum(debt) >= 0
-order by sum(debt) desc;
+select country_name,
+ cast(sum(debt)/1000000 as numeric (10,2)) as Total_principal_repayments_in_millions
+from assignment.international_debt_with_missing_values
+ where indicator_name Ilike '%Principal repayments%'
+ and country_name is not null
+ and debt is not null
+ group by country_name
+ order by Total_principal_repayments_in_millions desc
+ limit 1;
 
+ 
 --What is the most common debt indicator across all countries?
 select indicator_name,
-count(indicator_name)
-from international_debt_with_missing_values idwmv 
+count(indicator_name) as count_of_debt_indicator
+from assignment.international_debt_with_missing_values idwmv 
+where indicator_name is not null
 group by indicator_name
-order by count (indicator_name) desc;
-
+order by count(indicator_name) desc
+limit 1;
+ 
 --Identify any other key debt trends and summarize your findings.
-
-select *
-from international_debt_with_missing_values idwmv 
-
-
-
-
-
-
-
-
